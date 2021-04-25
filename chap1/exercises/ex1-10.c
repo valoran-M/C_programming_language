cdc7f303bc58d5ddd9cbf1513c3c305d10c0e5f7
Valeran-M <v.maytie85@orange.fr>
2021-04-25 17:39:33 +0200

    add ex1-10.c

---
+++ chap1/exercises/ex1-10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\t')
+            printf("\\t");
+        else if (c == '\b')
+            printf("\\b");
+        else if (c == '\\')
+            printf("\\");
+        else
+            putchar(c);
+    }
+}
