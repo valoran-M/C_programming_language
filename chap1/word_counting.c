2f69f855d87d2e0bff7815d36b3af242b06ea93b
Valeran-M <v.maytie85@orange.fr>
2021-04-25 17:49:27 +0200

    add word counting

---
+++ chap1/word_counting.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+void main()
+{
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF)
+    {
+        nc++;
+        if (c == '\n')
+            nl++;
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT)
+        {
+            state = IN;
+            nw++;
+        }
+    }
+    printf("nl = %d, nw = %d, nc = %d\n", nl, nw, nc);
+}
