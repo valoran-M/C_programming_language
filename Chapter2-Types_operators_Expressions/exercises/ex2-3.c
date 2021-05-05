c16f048163514ad9d1ea78f0751629d76ea0965c
Valeran-M <v.maytie85@orange.fr>
2021-05-05 14:03:23 +0200

    ex2-3.c

--- Chapter2-Types_operators_Expressions/exercises/ex2-3.c
+++ Chapter2-Types_operators_Expressions/exercises/ex2-3.c
@@ -10,30 +10,45 @@
 
 #define MAXCHAR 100
 
-int htoi(char s[]);
+unsigned int htoi(char s[], int len);
 int my_getline(char s[], int lim);
 
 void main()
 {
     char s[MAXCHAR];
-    int i;
+    unsigned int i;
+    int len;
 
-    my_getline(s, MAXCHAR);
-    i = htoi(s);
+    len = my_getline(s, MAXCHAR);
+    i = htoi(s, len);
     printf("%d\n", i);
+    printf("%x\n", i); // verification
 }
 
-int htoi(char s[])
+unsigned int htoi(char s[], int len)
 {
-    if (s[0] == '0' && (s[1] == 'X' || s[1] == 'X'))
+    unsigned int number = 0, pow = 1, place;
+    if (s[0] == '0' && (s[1] == 'x' || s[1] == 'X'))
     {
-        
+        for (place = len - 1; place > 1; place--)
+        {
+            if (s[place] >= '0' && s[place] <= '9')
+                number += (s[place] - '0') * pow;
+            else if (s[place] >= 'A' && s[place] <= 'F')
+                number += (s[place] - 'A' + 10) * pow;
+            else if (s[place] >= 'a' && s[place] <= 'f')
+                number += (s[place] - 'a' + 10) * pow;
+            else
+                printf("Error : %c is incompatible chractere\n", s[place]);
+            pow *= 16;
+        }
     }
     else
     {
-        printf("Error : begin with 0x / 0X");
+        printf("Error : begin with 0x / 0X\n");
         return 0;
     }
+    return number;
 }
 
 /* getline: read a line into s, return length */
@@ -43,11 +58,7 @@ int my_getline(char s[], int lim)
 
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
         s[i] = c;
-    if (c == '\n')
-    {
-        s[i] = c;
-        i++;
-    }
+
     s[i] = '\0';
     return i;
 }
