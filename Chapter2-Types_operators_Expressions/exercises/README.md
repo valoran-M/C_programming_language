f071a689cfd45a3a029e78cee0e071c9b8daceb2
Valeran-M <v.maytie85@orange.fr>
2021-05-06 17:36:07 +0200

    add ex2-6.c

--- Chapter2-Types_operators_Expressions/exercises/README.md
+++ Chapter2-Types_operators_Expressions/exercises/README.md
@@ -33,7 +33,7 @@
 
 ## Exercice 2.6
 
-    Write a function stebits(x, p, n, y) that return x with
+    Write a function setbits(x, p, n, y) that return x with
     the n bits tha begin at position p set to the rightmost
     n bits of y, leaving the other bits unchanged.
 
--- Chapter2-Types_operators_Expressions/exercises/ex2-6.c
+++ Chapter2-Types_operators_Expressions/exercises/ex2-6.c
@@ -1,8 +1,28 @@
 /**
  *  Exercice 2.6
  * 
- *  Write a function stebits(x, p, n, y) that return x with
+ *  Write a function setbits(x, p, n, y) that return x with
  * the n bits tha begin at position p set to the rightmost
  * n bits of y, leaving the other bits unchanged.
  *
  **/
+#include <stdio.h>
+
+unsigned setbits(unsigned x, int p, int n, unsigned y);
+
+void main()
+{
+    printf("%d\n", setbits(0707 , 15, 8, 070));
+}
+
+unsigned setbits(unsigned x, int p, int n, unsigned y)
+{
+    p++;
+    // xxxx0000xxxx
+    unsigned x_bits = x & ~(~(0 << n) << (p -  n));
+
+    // 0000yyyy0000
+    unsigned y_bits = (y & ~(~0 << n)) << (p -  n);
+
+    return x_bits | y_bits;
+}
