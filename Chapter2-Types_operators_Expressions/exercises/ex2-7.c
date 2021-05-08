2a026c7d2f5d77cd8f84a07742a9020142a15b6b
Valeran-M <v.maytie85@orange.fr>
2021-05-08 14:20:12 +0200

    ex 2-7 é-8

--- Chapter2-Types_operators_Expressions/exercises/ex2-7.c
+++ Chapter2-Types_operators_Expressions/exercises/ex2-7.c
@@ -6,3 +6,19 @@
  * bits of y, leaving the other bits unchanged.
  * 
  **/
+#include <stdio.h>
+
+unsigned invert(unsigned x, int p, int n);
+
+void main()
+{
+    printf("%u\n", invert(0b000000, 2, 1)); // 4
+    printf("%u\n", invert(0b000000, 3, 1)); // 8
+    printf("%u\n", invert(0b000000, 4, 1)); // 16
+
+}
+
+unsigned invert(unsigned x, int p, int n)
+{
+    return x ^ ((~(~0<<n))<< p+1-n);
+}
--- Chapter2-Types_operators_Expressions/exercises/ex2-8.c
+++ Chapter2-Types_operators_Expressions/exercises/ex2-8.c
@@ -5,3 +5,25 @@
  * integer x rotated to the right by n bit position
  *
  **/
+
+#include <stdio.h>
+
+unsigned reightrot(unsigned x, int n);
+
+void main()
+{
+    printf("%u\n", reightrot(0b00011, 0));
+}
+
+unsigned reightrot(unsigned x, int n)
+{
+    while (n > 0)
+    {
+        if (x & 1)
+            x = (x >> 1) | ~(~0 >> 1);
+        else
+            x = x >> 1;
+        --n;
+    }
+    return x;
+}
