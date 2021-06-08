fa1d5734801529dd296cbba541df1b3ebf2035d1
Valeran-M <v.maytie85@orange.fr>
2021-06-08 15:03:14 +0200

    add ex4.7

--- Chapter4-Functions_And_Program_Strucutre/exercises/ex4-7.c
+++ Chapter4-Functions_And_Program_Strucutre/exercises/ex4-7.c
@@ -8,3 +8,51 @@
  * 
  **/
 
+#include <stdio.h>
+#include <string.h>
+
+#define BUFSIZE 100
+#define MAXLINE 10000
+
+int getch(void);
+void ungetch(int);
+void ungets(char[]);
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int main(void)
+{
+    int c, i;
+    char s[MAXLINE];
+
+    for (i = 0; (s[i] = getch()) != '\n'; i++)
+        ;
+    s[++i] = '\n';
+    s[i] = '\0';
+    ungets(s);
+
+    while ((c = getch()) != EOF)
+        putchar(c);
+    return 0;
+}
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
+
+void ungets(char s[])
+{
+    int i;
+    for (i = strlen(s) - 1; i >= 0; --i)
+        ungetch(s[i]);
+}
