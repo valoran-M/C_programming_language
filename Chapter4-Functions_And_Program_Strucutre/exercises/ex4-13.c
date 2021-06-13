2e4c9b937eb97764c94799f6044335d1c56edd10
Valeran-M <v.maytie85@orange.fr>
2021-06-13 17:05:51 +0200

    add ex4.13

---
+++ Chapter4-Functions_And_Program_Strucutre/exercises/ex4-13.c
@@ -0,0 +1,38 @@
+/**
+ * Exercice 4.12:
+ * 
+ *  Write a recursive version of the function reverse(s),
+ * which reverses the string s int place. 
+ * 
+ **/
+
+#include <stdio.h>
+
+void revers(char s[], int size);
+
+int main()
+{
+    char al[] = "abcdefghijklmnopqrstuvwxyz";
+    printf("%s\n", al);
+    revers(al, 26);
+    printf("%s\n", al);
+
+    return 0;
+}
+
+void revers(char s[], int size)
+{
+    void reversr(int pos, char s[], int max);
+    reversr(0, s, size - 1);
+}
+
+void reversr(int pos, char s[], int max)
+{
+    if(pos > max / 2)
+        return;
+
+    char temp = s[pos];
+    s[pos] = s[max - pos];
+    s[max - pos] = temp;
+    reversr(pos + 1, s, max);
+}
