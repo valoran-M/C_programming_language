2cb8a2dc868a966b3556d1863784a96ebcb8a9f1
Valeran-M <v.maytie85@orange.fr>
2021-06-19 17:19:16 +0200

    add ex 5.5

--- Chapter5-Pointeurs_and_Arrays/exercises/ex5-04.c
+++ Chapter5-Pointeurs_and_Arrays/exercises/ex5-04.c
@@ -7,3 +7,39 @@
  * 
  **/
 
+#include <stdio.h>
+#include <string.h>
+
+int strend(char *s, char *t);
+
+int main()
+{
+    char s[] = "test";
+    char t1[] = "test";
+    char t2[] = "tests";
+
+    if(strend(s, t1))
+        printf("t1 - good\n");
+
+    if(!strend(s, t2))
+        printf("t2 - good\n");
+
+    return 0;
+}
+
+int strend(char *s, char *t)
+{
+    int size_s = strlen(s);
+    int size_t = strlen(t);
+
+    s += size_s;
+    t += size_t;
+
+    while (size_t && (*s-- == *t--))
+        --size_t;
+
+    if (size_t)
+        return 0;
+
+    return 1;
+}
