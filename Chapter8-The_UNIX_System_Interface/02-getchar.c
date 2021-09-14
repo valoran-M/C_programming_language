c6598a73c29678ab33ba0f6fa94a2e77a3c14660
valoran-M <contact-me@valeran-maytie.com>
2021-09-14 22:05:40 +0200

    update getchar

--- Chapter8-The_UNIX_System_Interface/02-getchar.c
+++ Chapter8-The_UNIX_System_Interface/02-getchar.c
@@ -4,9 +4,17 @@
 /* getchar unbuffered single character input */
 int getchar(void)
 {
-    char c;
+    static char buf[BUFSIZ];
+    static char *bufp = buf;
+    static int n = 0;
 
-    return (read(0, &c, 1) == 1) ? (unsigned char)c : EOF;
+    if (n == 0)
+    {
+        n = read(0, buf, sizeof buf);
+        bufp = buf;
+    }
+
+    return (--n >= 0) ? (unsigned char)*bufp++ : EOF;
 }
 
 int main(int argc, char const *argv[])
