69ee6f5ee126ab72786eed50104f593fda87d559
valoran-M <contact-me@valeran-maytie.com>
2021-09-14 18:15:00 +0200

    update ex 6.4

--- Chapter6-Structures/exercises/ex6-04.c
+++ Chapter6-Structures/exercises/ex6-04.c
@@ -17,7 +17,37 @@ struct key
     char *word;
     int count;
 } keytab[] = {
-    {"auto", 0}, {"break", 0}, {"case", 0}, {"char", 0}, {"enum", 0}, {"extern", 0}, {"float", 0}, {"for", 0}, {"goto", 0}, {"if", 0}, {"int", 0}, {"long", 0}, {"register", 0}, {"return", 0}, {"short", 0}, {"signed", 0}, {"const", 0}, {"continue", 0}, {"default", 0}, {"double", 0}, {"else", 0}, {"sizeof", 0}, {"static", 0}, {"struct", 0}, {"switch", 0}, {"typedef", 0}, {"union", 0}, {"unsigned", 0}, {"void", 0}, {"volatile", 0}, {"while", 0}};
+    {"auto", 0},
+    {"break", 0},
+    {"case", 0},
+    {"char", 0},
+    {"enum", 0},
+    {"extern", 0},
+    {"float", 0},
+    {"for", 0},
+    {"goto", 0},
+    {"if", 0},
+    {"int", 0},
+    {"long", 0},
+    {"register", 0},
+    {"return", 0},
+    {"short", 0},
+    {"signed", 0},
+    {"const", 0},
+    {"continue", 0},
+    {"default", 0},
+    {"double", 0},
+    {"else", 0},
+    {"sizeof", 0},
+    {"static", 0},
+    {"struct", 0},
+    {"switch", 0},
+    {"typedef", 0},
+    {"union", 0},
+    {"unsigned", 0},
+    {"void", 0},
+    {"volatile", 0},
+    {"while", 0}};
 
 #define NKEYS (sizeof keytab / sizeof keytab[0])
 
